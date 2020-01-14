f14b3728be8fbeac3fa8dee057693c21782a6caf
Evgeny <evsmanko@gmail.com>
2020-01-14 13:09:21 +0300

    update VK SDK version

--- app/src/main/java/com/example/allianceapp/MainActivity.java
+++ app/src/main/java/com/example/allianceapp/MainActivity.java
@@ -1,12 +1,12 @@
 package com.example.allianceapp;
 
 import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
-
-import com.vk.sdk.util.VKUtil;
-
+import com.vk.api.sdk.utils.VKUtils;
 import java.util.Arrays;
+
+
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        String[] fingerprints = VKUtil.getCertificateFingerprint(this, this.getPackageName());
+        String[] fingerprints = VKUtils.getCertificateFingerprint(this, this.getPackageName());
         System.out.println(Arrays.asList(fingerprints));
     }
 }
